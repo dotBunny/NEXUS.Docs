9c7e47f7749d1a3bb3531a0332b4b9f93fa3a245
Matthew Davey <reapazor@gmail.com>
2025-07-29 21:46:06 -0400

    Not great having the content in this branch

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -11,8 +11,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'NEXUS Framework',
-  tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
+  tagline: 'A battle-tested collection of game-ready plugins for Unreal Engine.',
+  favicon: 'assets/favicon/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
@@ -21,9 +21,11 @@ const config = {
 
   // Set the production url of your site here
   url: 'https://nexus-framework.com',
+  
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
+  trailingSlash: true,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -41,6 +43,7 @@ const config = {
     locales: ['en'],
   },
 
+
   presets: [
     [
       'classic',
@@ -51,7 +54,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/dotBunny/NEXUS.Docs/tree/main/docusaurus/',
         },
         blog: false,
         theme: {
@@ -64,8 +67,10 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      respectPrefersColorScheme: true,
+
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'assets/images/social/social-square.png',
       navbar: {
         title: 'NEXUS Framework',
         logo: {
@@ -148,6 +153,43 @@ const config = {
         theme: prismThemes.github,
         darkTheme: prismThemes.dracula,
       },
+      algolia: {
+        // The application ID provided by Algolia
+        appId: 'D8GP244DEM',
+
+        // Public API key: it is safe to commit it
+        apiKey: 'bd75718f03cda407bf3d9fb59f637d96',
+
+        indexName: 'nexus_framework_com_d8gp244dem_articles',
+
+        // Optional: see doc section below
+        contextualSearch: true,
+
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        externalUrlRegex: 'external\\.com|domain\\.com',
+
+        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        replaceSearchResultPathname: {
+          from: '/docs/', // or as RegExp: /\/docs\//
+          to: '/',
+        },
+
+        // Optional: Algolia search parameters
+        searchParameters: {},
+
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: 'search',
+
+        // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
+        insights: false,
+
+        //... other Algolia params
+      },
+
+      markdown: {
+        mermaid: true,
+      },
+
     }),
 };
 
