a91f350912ff749130fe53bc1789b16c091065c9
Matthew Davey <reapazor@gmail.com>
2025-08-11 16:55:16 -0400

    Area owners

---
+++ src/components/ContributorLink/index.tsx
@@ -0,0 +1,25 @@
+import type { ReactNode } from 'react';
+
+interface IContributor {
+  firstName: string;
+  lastName: string;
+  alias: string;
+  email: string;
+  social: string;
+}
+
+var Contributors: { [id: string]: IContributor; } = {
+  "reapazor": {
+    firstName: "Matthew", lastName: "Davey",
+    alias: "reapazor",
+    email: "reapazor@gmail.com",
+    social: "https://reapazor.com/"
+  },
+};
+
+
+export default function ContributorLink({ id }): ReactNode {
+  return (
+    Contributors[id].alias
+  )
+}
--- src/components/PluginDetails/index.tsx
+++ src/components/PluginDetails/index.tsx
@@ -1,25 +1,21 @@
 import type { ReactNode } from 'react';
 import styles from './styles.module.css';
+import ContributorLink from '../ContributorLink'
 
-type PluginDetailsItem = {
-  icon: string;
-  iconAlt: string;
-  initialVersion: string;
-};
-
-export default function PluginDetails({ icon, iconAlt, initialVersion }: PluginDetailsItem): ReactNode {
+export default function PluginDetails({ icon, moduleName, shortName, initialRelease, owner, children }): ReactNode {
 
   return (
     <div className={styles.pluginDetails}>
-      <div className={styles.pluginDetails}>
-        <img src={icon} alt={iconAlt} />
-      </div>
-      <div className={styles.pluginDetailsContainer}>
-        <div className={styles.pluginDetailsRow}>
-          <p>Initial Version:</p>
-          <p>{initialVersion}</p>
-        </div>
-      </div>
+      <div className={styles.pluginDetailsIcon}><img src={icon} alt={moduleName} /></div>
+      <dl>
+        <dt>Module Name:</dt>
+        <dd>{moduleName}</dd>
+        <dt>Initial Release:</dt>
+        <dd>{initialRelease}</dd>
+        {children}
+        <dt>Area Owner:</dt>
+        <dd><ContributorLink id={owner} /></dd>
+      </dl>
     </div>
   );
 }
