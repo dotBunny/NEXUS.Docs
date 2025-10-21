4769f98e043ff312460295ddb62cbda24b986845
Matthew Davey <reapazor@gmail.com>
2025-10-21 15:12:59 -0400

    Correct verbage around dev branch

--- src/components/DevBanner/index.tsx
+++ src/components/DevBanner/index.tsx
@@ -5,7 +5,7 @@ export default function DevBanner({ }): ReactNode {
     <div className="devBanner">
       <div className="devBannerTitle"><span className="devBannerIcon-left">🚧</span>Under Development<span className="devBannerIcon-right">🚧</span></div>
       <div className="devBannerMessage">
-        <p>This base of this functionality is currently only available in the <code><a href="https://github.com/dotBunny/NEXUS/tree/dev">dev</a></code> branch as it is in <strong>active</strong> development.</p>
+        <p>The base of this functionality is currently only available in the <code><a href="https://github.com/dotBunny/NEXUS/tree/dev">dev</a></code> branch as it is in <strong>active</strong> development.</p>
         <p>Check out the <a href="/community/roadmap/">Roadmap</a>, or the work-in-process <a href="https://github.com/orgs/dotBunny/projects/6/views/1">Overwatch</a> board to see whats going on!</p>
       </div>
     </div>
